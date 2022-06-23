45738e2f03579eee7b824ec8fc32413a8b8452da
gviloria001 <viloria.gerardo@yahoo.com>
2022-06-23 00:03:34 -0700

    Update DB.js

--- client/src/pages/DB.js
+++ client/src/pages/DB.js
@@ -5,24 +5,24 @@ export default function DB() {
 
     return (
         <div className='site-layout'>
-      <div className='header'>
-        Popcorn Theaters
-      </div>
-      <div className='report-bar'> 
-        <button onClick={() => setVisible(!visible)}>{visible ? 'Show' : 'Hide'}
-        </button>
-        {visible && 
-            <button>
-                My element
-            </button>
-        }
-      </div>
-      <div className='site-content'> 
-        
-      </div>
-      <div className='footer'>
-        All Rights Reversed
-      </div>
-    </div>
+            <div className='header'>
+                Popcorn Theaters
+            </div>
+            <div className='report-bar'>
+                <button onClick={() => setVisible(!visible)}>{visible ? 'Show' : 'Hide'}
+                </button>
+                {visible &&
+                    <button>
+                        My element
+                    </button>
+                }
+            </div>
+            <div className='site-content'>
+
+            </div>
+            <div className='footer'>
+                All Rights Reversed
+            </div>
+        </div>
     )
 }
